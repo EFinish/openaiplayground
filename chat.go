f8ca34c9a0d4f16a2371471032aed08e3f522488
EFinish <EFinish@gmail.com>
2024-12-03 04:13:57 +0100

    update chat to allow for continuous conversation

--- chat.go
+++ chat.go
@@ -11,6 +11,8 @@ import (
 	"github.com/openai/openai-go/option"
 )
 
+const IMDONE = "IMDONE"
+
 func beginChat() {
 	apiKey := os.Getenv("OPENAI_API_KEY")
 	client := openai.NewClient(
@@ -19,19 +21,34 @@ func beginChat() {
 
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print(string(ColorNarrator), STARLINE+"The chat has begun! You need to start the conversation though... go ahead and introduce yourself.:\n")
-	fmt.Print(string(ColorUser))
-	input, _ := reader.ReadString('\n')
-	input = strings.Replace(input, "\n", "", -1)
+	fmt.Print(string(ColorNarrator), STARLINE+"The chat has begun! You need to start the conversation though... go ahead and introduce yourself. Type IMDONE to finish chatting and return to the main menu.\n")
 
-	chatCompletion, err := client.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
-		Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
-			openai.UserMessage(input),
-		}),
-		Model: openai.F(openai.ChatModelGPT4o),
-	})
-	if err != nil {
-		panic(err.Error())
+	userIsDone := false
+	chatHistory := openai.ChatCompletionNewParams{
+		Messages: openai.F([]openai.ChatCompletionMessageParamUnion{}),
+		Model:    openai.F(openai.ChatModelGPT4oMini),
 	}
-	fmt.Print(string(ColorMachine), chatCompletion.Choices[0].Message.Content+"\n")
+
+	for !userIsDone {
+		fmt.Print(string(ColorUser))
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		if userInput == IMDONE {
+			userIsDone = true
+			break
+		}
+
+		chatHistory.Messages.Value = append(chatHistory.Messages.Value, openai.UserMessage(userInput))
+
+		chatCompletion, err := client.Chat.Completions.New(context.TODO(), chatHistory)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		chatHistory.Messages.Value = append(chatHistory.Messages.Value, chatCompletion.Choices[0].Message)
+
+		fmt.Print(string(ColorMachine), chatCompletion.Choices[0].Message.Content+"\n")
+	}
 }
